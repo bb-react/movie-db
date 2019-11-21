5e8c9237fb607dde82e0862d72ff664440393222
Boris Brtáň <39240194+boris-brtan@users.noreply.github.com>
2019-11-21 22:16:09 +0100

    adding api for OMDB services

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+const apiURL = 'http://www.omdbapi.com/?apikey=cb5da875';
+
+export default {
+    fetchMovie: async (id) => {
+        const response = await fetch(`${apiURL}&i=${id}`);
+        const movie = await response.json();
+
+        return movie;
+    },
+    fetchMovies: async (search, page) => {
+        const response = await fetch(`${apiURL}&s=${search}&page=${page}`);
+        const movie = await response.json();
+
+        return movie;
+    },
+}
