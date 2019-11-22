1cf6314c1cd362e6a2d451d044a62a3931a8f445
bb-react <bb-movie@post.com>
2019-11-22 08:42:37 +0100

    Updates

---
+++ precache-manifest.862941d1b5df892cd9ece127d1181027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55883f7dabb92c9aadad9c13ad3399e1",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "ad1a4f5460fd09a7e9a8",
+    "url": "/movie-db/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c8b185f2f5e4bbf83e82",
+    "url": "/movie-db/static/js/2.22064c39.chunk.js"
+  },
+  {
+    "revision": "ad1a4f5460fd09a7e9a8",
+    "url": "/movie-db/static/js/main.8781f29c.chunk.js"
+  },
+  {
+    "revision": "eacf66fddf9478e868ca",
+    "url": "/movie-db/static/js/runtime-main.84dfda5d.js"
+  }
+]);
